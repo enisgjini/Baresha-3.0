6993f2cf2a1e1265c2ab9479a4814095246129dd
Enis Gjini <gjinienis148@gmail.com>
2023-02-08 11:06:14 +0100

    #

--- dataYT.php
+++ dataYT.php
@@ -74,120 +74,16 @@
                     <div class="col w-50">
                         <div class="card ">
                             <div id="chart" class="rounded rounded-5 py-4"></div>
-                            <script>
-                                const apiKey = "AIzaSyAztDJAJfQro8zUj5cKcecJjLjovAgA6S0";
-                                let channelId = "UCV6ZBT0ZUfNbtZMbsy-L3CQ";
-                                let videoContainer = document.getElementById("videoContainer");
-
-                                function updateChannelId(value) {
-                                    channelId = value;
-                                    fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`)
-                                        .then(response => response.json())
-                                        .then(data => {
-                                            console.log(data);
-                                            const viewCount = data.items[0].statistics.viewCount;
-                                            const subscriberCount = data.items[0].statistics.subscriberCount;
-                                            const videoCount = data.items[0].statistics.videoCount;
-
-                                            Highcharts.chart("chart", {
-                                                chart: {
-                                                    type: "area"
-                                                },
-                                                title: {
-                                                    text: "Analiza e kanalit - YouTube"
-                                                },
-                                                xAxis: {
-                                                    categories: ['Abonent', 'Videot']
-                                                },
-                                                yAxis: {
-                                                    title: {
-                                                        text: 'Vlerat'
-                                                    }
-                                                },
-                                                series: [{
-                                                    name: "Te dhenat",
-                                                    data: [parseInt(subscriberCount), parseInt(videoCount)]
-                                                }]
-                                            });
 
 
-                                        });
-
-                                    const selectElement = document.querySelector("select");
-                                    const shfaqjaEEmrit = "Emri i artistit : " + selectElement.options[selectElement.selectedIndex].text;
-                                    document.getElementById("selectedOptionText").innerHTML = shfaqjaEEmrit;
-
-
-                                    const kanalID = "ID e kanalit : " + selectElement.options[selectElement.selectedIndex].value;
-                                    document.getElementById("selectedOptionText2").innerHTML = kanalID;
-
-
-                                }
-                            </script>
                         </div>
                     </div>
                     <div class="col w-50">
-                        <h2>Enis</h2>
-                        <div id="revenue"></div>
-                        <div id="card" class="card">
-                            <div id="header">Card Header</div>
-                            <div id="content">Card Content</div>
-                            <button id="deleteBtn">Delete</button>
+                        <div class="card ">
+                            <div id="chart2" class="rounded rounded-5 py-4"></div>
                         </div>
-
-
-
-
                     </div>
-                    <script>
-                        // Replace {YOUR_CLIENT_ID} and {YOUR_REDIRECT_URI} with your OAuth 2.0 client ID and redirect URI
-                        // 832897216603-alf98kgeje3867l1k8cie85t72oc1aj9.apps.googleusercontent.com
-                        // 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
-                        const clientId = '832897216603-alf98kgeje3867l1k8cie85t72oc1aj9.apps.googleusercontent.com';
-                        const redirectUri = 'http://localhost/Baresha_3.0/dataYT.php';
 
-                        // Request user authorization
-                        const authUrl = new URL('https://accounts.google.com/o/oauth2/v2/auth');
-                        authUrl.searchParams.set('client_id', clientId);
-                        authUrl.searchParams.set('redirect_uri', redirectUri);
-                        authUrl.searchParams.set('scope', 'https://www.googleapis.com/auth/yt-analytics.readonly');
-                        authUrl.searchParams.set('response_type', 'code');
-
-                        // Check if the user has granted access
-                        if (!window.location.search.includes('code')) {
-                            window.location.href = authUrl.toString();
-                        } else {
-                            // Extract the authorization code from the query string of the redirect URL
-                            const code = new URL(window.location.href).searchParams.get('code');
-
-                            // Exchange the authorization code for an access token
-                            fetch('https://oauth2.googleapis.com/token', {
-                                    method: 'POST',
-                                    headers: {
-                                        'Content-Type': 'application/x-www-form-urlencoded'
-                                    },
-                                    body: new URLSearchParams({
-                                        code,
-                                        client_id: clientId,
-                                        redirect_uri: redirectUri,
-                                        grant_type: 'authorization_code',
-                                    }),
-                                })
-                                .then(response => response.json())
-                                .then(token => {
-                                    // Make the authorized API request
-                                    fetch('https://youtubeanalytics.googleapis.com/v2/reports', {
-                                            headers: {
-                                                'Authorization': `Bearer ${token.access_token}`
-                                            },
-                                        })
-                                        .then(response => response.json())
-                                        .then(data => {
-                                            console.log(data);
-                                        });
-                                });
-                        }
-                    </script>
                 </div>
             </div>
         </div>
@@ -195,68 +91,73 @@
 </div>
 
 
-
-<?php include 'partials/footer.php' ?>
-
-
 <script>
-    const card = document.querySelector("#card");
-    const deleteBtn = document.querySelector("#deleteBtn");
-    const showBtn = document.querySelector("#showBtn");
+    const apiKey = "AIzaSyAjEtD_5L5nvy-iihOk7soj9QSBJVhIF2Q";
+    let channelId = "UCV6ZBT0ZUfNbtZMbsy-L3CQ";
+    let videoContainer = document.getElementById("videoContainer");
 
-    let isDragging = false;
-    let currentX;
-    let currentY;
-    let initialX;
-    let initialY;
-    let xOffset = 0;
-    let yOffset = 0;
+    function updateChannelId(value) {
+        channelId = value;
+        fetch(`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelId}&key=${apiKey}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                const viewCount = data.items[0].statistics.viewCount;
+                const subscriberCount = data.items[0].statistics.subscriberCount;
+                const videoCount = data.items[0].statistics.videoCount;
 
-    card.addEventListener("mousedown", dragStart);
-    card.addEventListener("mouseup", dragEnd);
-    card.addEventListener("mousemove", drag);
+                Highcharts.chart("chart", {
+                    chart: {
+                        type: "column"
+                    },
+                    title: {
+                        text: "Analiza e kanalit - YouTube"
+                    },
+                    xAxis: {
+                        categories: ['Abonent']
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Vlerat'
+                        }
+                    },
+                    series: [{
+                        name: "Te dhenat",
+                        data: [parseInt(subscriberCount)]
+                    }]
+                });
 
-    deleteBtn.addEventListener("click", deleteCard);
-    showBtn.addEventListener("click", showCard);
+                Highcharts.chart("chart2", {
+                    chart: {
+                        type: "column"
+                    },
+                    title: {
+                        text: "Analiza e kanalit - YouTube"
+                    },
+                    xAxis: {
+                        categories: ['Videot']
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Vlerat'
+                        }
+                    },
+                    series: [{
+                        name: "Te dhenat",
+                        data: [parseInt(videoCount)]
+                    }]
+                });
+            });
 
-    function dragStart(e) {
-        initialX = e.clientX - xOffset;
-        initialY = e.clientY - yOffset;
+        const selectElement = document.querySelector("select");
+        const shfaqjaEEmrit = "Emri i artistit : " + selectElement.options[selectElement.selectedIndex].text;
+        document.getElementById("selectedOptionText").innerHTML = shfaqjaEEmrit;
 
-        isDragging = true;
-    }
 
-    function dragEnd(e) {
-        initialX = currentX;
-        initialY = currentY;
+        const kanalID = "ID e kanalit : " + selectElement.options[selectElement.selectedIndex].value;
+        document.getElementById("selectedOptionText2").innerHTML = kanalID;
 
-        isDragging = false;
-    }
 
-    function drag(e) {
-        if (isDragging) {
-            e.preventDefault();
-            currentX = e.clientX - initialX;
-            currentY = e.clientY - initialY;
-
-            xOffset = currentX;
-            yOffset = currentY;
-
-            setTranslate(currentX, currentY, card);
-        }
-    }
-
-    function setTranslate(xPos, yPos, el) {
-        el.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
-    }
-
-    function deleteCard() {
-        card.style.display = "none";
-        showBtn.style.display = "block";
-    }
-
-    function showCard() {
-        card.style.display = "block";
-        showBtn.style.display = "none";
     }
 </script>
+<?php include 'partials/footer.php' ?>
--- partials/navbar.php
+++ partials/navbar.php
@@ -20,12 +20,7 @@
           <input type="text" class="form-control" placeholder="Search now" aria-label="search" aria-describedby="search">
         </div>
       </li>
-      <li>
-        <button id="button-id" class="btn btn-light"><i class="fi fi-rr-brightness" id="icon-id"></i></button>
-      </li>
-      <li>
-        <button class="btn btn-primary" id="skinToggler">Toggle skin</button>
-      </li>
+
     </ul>
     <ul class="navbar-nav navbar-nav-right">
       <li class="nav-item dropdown me-1">
@@ -141,37 +136,3 @@
     </button>
   </div>
 </nav>
-
-
-<script>
-  window.onload = function() {
-    const button = document.getElementById('button-id');
-    const icon = document.getElementById('icon-id');
-    const otherElement = document.getElementById('sss');
-
-    let isDarkMode = false;
-
-    button.addEventListener('click', function() {
-      if (isDarkMode) {
-
-        icon.classList.remove('fi-rr-moon');
-        icon.classList.add('fi-rr-brightness');
-        otherElement.style.backgroundColor = 'white';
-        otherElement.classList.remove("text-warning");
-        otherElement.classList.add("text-dark");
-      } else {
-
-        icon.classList.remove('fi-rr-brightness');
-        icon.classList.add('fi-rr-moon');
-        otherElement.style.color = 'white';
-        otherElement.style.backgroundColor = 'black';
-        otherElement.classList.remove("text-dark");
-        otherElement.classList.add("text-warning");
-      }
-
-      isDarkMode = !isDarkMode;
-    });
-  };
-
-  
-</script>
